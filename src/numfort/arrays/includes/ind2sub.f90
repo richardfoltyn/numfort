b78d76b5c3aa3b82f6e7700fe7cefdb17cc3455d
Richard Foltyn <richard.foltyn@gmail.com>
2016-06-26 22:06:34 +0200

    ind2sub: fix bug

--- src/numfort/arrays/includes/ind2sub.f90
+++ src/numfort/arrays/includes/ind2sub.f90
@@ -2,7 +2,7 @@ integer (INTSIZE), intent(in), dimension(:) :: shp
 integer (INTSIZE), intent(in), dimension(:) :: lin_indices
 integer (INTSIZE), intent(out), dimension(:,:) :: sub_indices
 
-integer (INTSIZE), dimension(size(shape(sub_indices))) :: stride
+integer (INTSIZE), dimension(size(shp)) :: stride
 integer (INTSIZE) :: i, j, n, d, rnk, stride_j, rem_i, s_ij
 integer (INTSIZE), dimension(:), allocatable :: rem
 
@@ -19,22 +19,25 @@ d = size(sub_indices, 2)
 ! automatically deallocated on procedure exit
 allocate (rem(n), source=lin_indices)
 
+rem = rem - 1
+
 do j = d, 2, -1
     stride_j = stride(j)
-    ! current remainder for i
-    rem_i = rem(i)
     do i = 1, n
+        ! current remainder for i
+        rem_i = rem(i)
         ! keep only integer part of expression; no need to apply floor()
         s_ij = rem_i / stride_j
         ! adjust remainder for processed dimension
         rem_i = rem_i - s_ij * stride_j
         ! adjust index to start at 1
         sub_indices(i, j) = s_ij + 1
+        rem(i) = rem_i
     end do
 end do
 
 ! index on first dimension is just the remainder after all higher dimensions
 ! have been subtracted out
-sub_indices(:, 1) = rem
+sub_indices(:, 1) = rem + 1
 
 
