67fb314f5389c633a0a51aa6d1a8a62c7bb51a67
Richard Foltyn <richard.foltyn@gmail.com>
2016-08-21 18:15:26 +0200

    Fix missing implementation of workspace changes

--- src/numfort/common/include/workspace_assert_allocated_impl.f90
+++ src/numfort/common/include/workspace_assert_allocated_impl.f90
@@ -1,6 +1,7 @@
 ! Type-independent part of workspace_assert_allocated
 
 intent(in out) :: arr
+integer, intent(in out) :: n_attr
 integer, intent(in), optional :: n
 
 allocatable :: arr, tmp
@@ -8,8 +9,10 @@ allocatable :: arr, tmp
 if (present(n)) then
     if (.not. allocated(arr)) then
         allocate (arr(n))
+        n_attr = n
     else if (size(arr) < n) then
         allocate (tmp(n))
         call move_alloc (tmp, arr)
+        n_attr = n
     end if
 end if
