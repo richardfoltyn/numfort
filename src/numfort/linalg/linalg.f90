d0926057fdfabeff7a5089d6bb526ac9deef8781
Richard Foltyn <richard.foltyn@gmail.com>
2016-08-29 14:34:49 +0200

    Linalg: Rewrite matrix inversion subroutine

--- src/numfort/linalg/linalg.f90
+++ src/numfort/linalg/linalg.f90
@@ -1,52 +1,26 @@
 module numfort_linalg
 
-    use iso_fortran_env, only : real64, int64
+    use iso_fortran_env
 
     implicit none
     private
 
     interface inv
-        procedure :: inv_real64
+        procedure :: inv_real64, inv_real32
     end interface
 
     public :: inv
 
 contains
 
-! Returns the inverse of a matrix calculated by finding the LU
-! decomposition.  Depends on LAPACK.
-function inv_real64(A) result(Ainv)
-  real(real64), dimension(:,:), intent(in) :: A
-  real(real64), dimension(size(A,1),size(A,2)) :: Ainv
+subroutine inv_real64(A, Ainv, info)
+    integer, parameter :: PREC = real64
+    include "include/inv_impl.f90"
+end subroutine
 
-  real(real64), dimension(size(A,1)) :: work  ! work array for LAPACK
-  integer, dimension(size(A,1)) :: ipiv   ! pivot indices
-  integer :: n, info
-
-  ! External procedures defined in LAPACK
-  external DGETRF
-  external DGETRI
-
-  ! Store A in Ainv to prevent it from being overwritten by LAPACK
-  Ainv = A
-  n = size(A,1)
-
-  ! DGETRF computes an LU factorization of a general M-by-N matrix A
-  ! using partial pivoting with row interchanges.
-  call DGETRF(n, n, Ainv, n, ipiv, info)
-
-  if (info /= 0) then
-     stop 'Matrix is numerically singular!'
-  end if
-
-  ! DGETRI computes the inverse of a matrix using the LU factorization
-  ! computed by DGETRF.
-  call DGETRI(n, Ainv, n, ipiv, work, n, info)
-
-  if (info /= 0) then
-     stop 'Matrix inversion failed!'
-  end if
-end function
-
+subroutine inv_real32(A, Ainv, info)
+    integer, parameter :: PREC = real32
+    include "include/inv_impl.f90"
+end subroutine
 
 end module
