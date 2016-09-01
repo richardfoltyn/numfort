6449026936f1c95f6701a94754c0dab67309745c
Richard Foltyn <richard.foltyn@gmail.com>
2016-09-01 17:53:41 +0200

    Allocate workspace only if size > 0

--- src/numfort/common/include/workspace_assert_allocated_impl.f90
+++ src/numfort/common/include/workspace_assert_allocated_impl.f90
@@ -7,12 +7,14 @@ integer, intent(in), optional :: n
 allocatable :: arr, tmp
 
 if (present(n)) then
-    if (.not. allocated(arr)) then
-        allocate (arr(n))
-        n_attr = n
-    else if (size(arr) < n) then
-        allocate (tmp(n))
-        call move_alloc (tmp, arr)
-        n_attr = n
+    if (n > 0) then
+        if (.not. allocated(arr)) then
+            allocate (arr(n))
+            n_attr = n
+        else if (size(arr) < n) then
+            allocate (tmp(n))
+            call move_alloc (tmp, arr)
+            n_attr = n
+        end if
     end if
 end if
