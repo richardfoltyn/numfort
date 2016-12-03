a36b9085035c97fe10b1db7535c0d292974439d0
Richard Foltyn <richard.foltyn@gmail.com>
2016-12-03 10:51:37 +0100

    benchmarks: Add some normal cdf benchmarks

---
+++ src/benchmarks/stats/dnorm.f90
@@ -0,0 +1,38 @@
+program benchmark_dnorm
+
+    use iso_fortran_env
+    use numfort_arrays
+    use numfort_stats, only: norm
+    implicit none
+
+
+    call benchmark_cdf ()
+
+contains
+
+subroutine benchmark_cdf ()
+
+    integer, parameter :: N = 10000
+    integer :: i, repeat
+
+    real (real64), dimension(:,:), allocatable :: x, fx
+    real (real64), dimension(:,:), allocatable :: vals
+
+    repeat = 1000
+
+    allocate (x(N,N), fx(N,N), vals(N))
+
+    call linspace (vals, -1d5, 1d5)
+
+    do i = 1, N
+        x(:, i) = vals
+    end do
+
+    do i = 1, repeats
+        call norm%cdf (x, fx)
+    end do
+
+
+end subroutine
+
+end program
