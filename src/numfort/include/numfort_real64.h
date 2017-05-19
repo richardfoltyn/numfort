3606c780d1e7e1f94f9abf7a93a6c9425d73908e
Richard Foltyn <richard.foltyn@gmail.com>
2017-05-19 10:50:40 +0200

    Automatically generate prec-specific headers

--- src/numfort/include/numfort_real64.h
+++ src/numfort/include/numfort_real64.h
@@ -1,8 +1,38 @@
+
 /*
-    Define macros that transform code to real64 KIND
+    Include file for real64 precision. 
+    THIS FILE IS GENERATED AUTOMATICALLY BY gen_prec_headers.py, DO NOT MODIFY.
 */
+
+
 #ifdef __PREC
 #undef __PREC
 #endif
-
 #define __PREC real64
+
+/* BLAS routines */
+
+#ifdef __GEMM
+#undef __GEMM
+#endif
+#define __GEMM DGEMM
+
+/* LAPACK routines */
+
+#ifdef __GESVD
+#undef __GESVD
+#endif
+#define __GESVD DGESVD
+
+
+#ifdef __GELSD
+#undef __GELSD
+#endif
+#define __GELSD DGELSD
+
+
+#ifdef __GESDD
+#undef __GESDD
+#endif
+#define __GESDD DGESDD
+
