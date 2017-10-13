2bc1b4a53e33bc72447bfaac245ce5aa2e6d63af
Richard Foltyn <richard.foltyn@gmail.com>
2017-10-13 19:32:37 +0200

    integrate: export required workspace stuff from integrate module

--- src/numfort/integrate/integrate.f90
+++ src/numfort/integrate/integrate.f90
@@ -2,6 +2,8 @@ module numfort_integrate
 
     use numfort_common_enums
     use numfort_common_status
+    use numfort_common_workspace, only: workspace_real32, workspace_real64, &
+        workspace_finalize
     use numfort_integrate_basic
     use numfort_integrate_quadpack
 
