e2e6e2d142e9c8bef7e536ff3a38e711b889d3f6
Richard Foltyn <richard.foltyn@gmail.com>
2017-10-14 20:01:14 +0200

    optimize: Add CONTIGUOUS attribute to obj/constr. func interfaces

--- src/numfort/optimize/include/interfaces_spec.F90
+++ src/numfort/optimize/include/interfaces_spec.F90
@@ -2,30 +2,30 @@
 
 subroutine __APPEND(fvec_scalar_args,__PREC) (x, fx, fpx, args)
     import __PREC
-    real (__PREC), intent(in), dimension(:) :: x
+    real (__PREC), intent(in), dimension(:), contiguous :: x
     real (__PREC), intent(out), optional :: fx
-    real (__PREC), intent(out), dimension(:), optional :: fpx
+    real (__PREC), intent(out), dimension(:), contiguous, optional :: fpx
     real (__PREC), intent(in), dimension(:), optional :: args
 end subroutine
 
 subroutine __APPEND(fvec_vec_args,__PREC) (x, fx, fpx, args)
     import __PREC
-    real (__PREC), intent(in), dimension(:) :: x
-    real (__PREC), intent(out), dimension(:), optional :: fx
-    real (__PREC), intent(out), dimension(:,:), optional :: fpx
+    real (__PREC), intent(in), dimension(:), contiguous :: x
+    real (__PREC), intent(out), dimension(:), contiguous, optional :: fx
+    real (__PREC), intent(out), dimension(:,:), contiguous, optional :: fpx
     real (__PREC), intent(in), dimension(:), optional :: args
 end subroutine
 
 subroutine __APPEND(fvec_scalar,__PREC) (x, fx, fpx)
     import __PREC
-    real (__PREC), intent(in), dimension(:) :: x
+    real (__PREC), intent(in), dimension(:), contiguous :: x
     real (__PREC), intent(out), optional :: fx
-    real (__PREC), intent(out), dimension(:), optional :: fpx
+    real (__PREC), intent(out), dimension(:), contiguous, optional :: fpx
 end subroutine
 
 subroutine __APPEND(fvec_vec,__PREC) (x, fx, fpx)
     import __PREC
-    real (__PREC), intent(in), dimension(:) :: x
-    real (__PREC), intent(out), dimension(:), optional :: fx
-    real (__PREC), intent(out), dimension(:,:), optional :: fpx
+    real (__PREC), intent(in), dimension(:), contiguous :: x
+    real (__PREC), intent(out), dimension(:), contiguous, optional :: fx
+    real (__PREC), intent(out), dimension(:,:), contiguous, optional :: fpx
 end subroutine
