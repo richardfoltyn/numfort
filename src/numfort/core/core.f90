f224f69bdfb39459fddc76d211e3983fed045f85
Richard Foltyn <richard.foltyn@gmail.com>
2017-10-24 10:27:32 +0200

    core: Add SIGNUM function

--- src/numfort/core/core.f90
+++ src/numfort/core/core.f90
@@ -6,8 +6,8 @@ module numfort_core
     implicit none
     private
 
-    public :: cumsum, comb
-    public :: PI, PI_REAL32
+    public :: cumsum, comb, signum
+    public :: PI, PI_real32, PI_real64
 
     interface cumsum
         module procedure cumsum_1d_real64, cumsum_2d_real64, cumsum_3d_real64
@@ -21,9 +21,15 @@ module numfort_core
         module procedure comb_int32, comb_int64
     end interface
 
+    interface signum
+        module procedure signum_real32, signum_real64, signum_int32, signum_int64
+    end interface
+
     ! Math constants
-    real (real64), parameter :: PI = 3.141592653589793238462643383279502884d0
-    real (real32), parameter :: PI_REAL32 = 3.141592653589793238462643383279502884
+    real (real64), parameter :: PI_real64 = 3.141592653589793238462643383279502884d0
+    real (real32), parameter :: PI_real32 = 3.141592653589793238462643383279502884
+
+    real (real64), parameter :: PI = PI_real64
 
 contains
 
@@ -96,6 +102,83 @@ elemental function comb_int64 (n, k, repetition) result(res)
 end function
 
 
+!-------------------------------------------------------------------------------
+! SIGNUM: alternative SIGN function
+! Implement behavior that corresponds to what is found in numpy.
+
+elemental function signum_real64 (x) result(res)
+    !*  SIGNUM returns -1, 0 or 1 depending on whether x < 0, x == 0 or x > 0.
+    !   Note: Differs from intrinsic SIGN, as SIGN return 1 whenever x >= 0
+    !   and -1 otherwise.
+    integer, parameter :: PREC = real64
+    real (PREC), intent(in) :: x
+    real (PREC) :: res
+
+    if (x > 0.0_PREC) then
+        res = 1.0_PREC
+    else if (x < 0.0_PREC) then
+        res = -1.0_PREC
+    else
+        res = 0.0_PREC
+    end if
+end function
+
+
+elemental function signum_real32 (x) result(res)
+    !*  SIGNUM returns -1, 0 or 1 depending on whether x < 0, x == 0 or x > 0.
+    !   Note: Differs from intrinsic SIGN, as SIGN return 1 whenever x >= 0
+    !   and -1 otherwise.
+    integer, parameter :: PREC = real32
+    real (PREC), intent(in) :: x
+    real (PREC) :: res
+
+    if (x > 0.0_PREC) then
+        res = 1.0_PREC
+    else if (x < 0.0_PREC) then
+        res = -1.0_PREC
+    else
+        res = 0.0_PREC
+    end if
+end function
+
+
+elemental function signum_int32 (x) result(res)
+    !*  SIGNUM returns -1, 0 or 1 depending on whether x < 0, x == 0 or x > 0.
+    !   Note: Differs from intrinsic SIGN, as SIGN return 1 whenever x >= 0
+    !   and -1 otherwise.
+    integer, parameter :: INTSIZE = int32
+    integer (INTSIZE), intent(in) :: x
+    integer (INTSIZE)  :: res
+
+    if (x > 0_INTSIZE) then
+        res = 1_INTSIZE
+    else if (x < 0_INTSIZE) then
+        res = -1_INTSIZE
+    else
+        res = 0_INTSIZE
+    end if
+end function
+
+
+elemental function signum_int64 (x) result(res)
+    !*  SIGNUM returns -1, 0 or 1 depending on whether x < 0, x == 0 or x > 0.
+    !   Note: Differs from intrinsic SIGN, as SIGN return 1 whenever x >= 0
+    !   and -1 otherwise.
+    integer, parameter :: INTSIZE = int64
+    integer (INTSIZE), intent(in) :: x
+    integer (INTSIZE)  :: res
+
+    if (x > 0_INTSIZE) then
+        res = 1_INTSIZE
+    else if (x < 0_INTSIZE) then
+        res = -1_INTSIZE
+    else
+        res = 0_INTSIZE
+    end if
+end function
+
+
+!-------------------------------------------------------------------------------
 subroutine cumsum_1d_real64(x, res, axis)
 
     real (real64), intent(in), dimension(:) :: x
