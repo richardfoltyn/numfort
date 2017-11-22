0b4a12ea293daf3167bdcf89215da9796c6650e2
Richard Foltyn <richard.foltyn@gmail.com>
2017-11-22 19:13:30 +0100

    common: Fix illegal access to missing dummy in COPY_ALLOC

--- src/numfort/arrays/include/copy_alloc_3d_impl.f90
+++ src/numfort/arrays/include/copy_alloc_3d_impl.f90
@@ -1,15 +1,17 @@
 
 integer, dimension(ND) :: shp
 
-if (.not. shape_equal (src, dst) .and. allocated(dst)) then
-    deallocate (dst)
-end if
-
 if (present(src)) then
+    if (.not. shape_equal (src, dst) .and. allocated(dst)) then
+        deallocate (dst)
+    end if
+
     shp = shape(src)
     if (.not. allocated(dst)) then
         allocate (dst(shp(1),shp(2),shp(3)), source=src)
     else
         dst(:,:,:) = src
     end if
+else
+    if (allocated(dst)) deallocate (dst)
 end if
