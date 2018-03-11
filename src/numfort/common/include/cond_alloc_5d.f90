5078138ec4d15851cbc4d55e2ecfd2c7177cff78
Richard Foltyn <richard.foltyn@gmail.com>
2018-03-11 16:28:36 +0100

    common: Add COND_ALLOC for 5d arrays

---
+++ src/numfort/common/include/cond_alloc_5d.f90
@@ -0,0 +1,24 @@
+
+! Implementation for 1d-arrays and array SOURCE arguments
+
+integer, intent(out), optional :: stat
+    !*  Status flag. If present, it is set to -1 if no allocation is required
+    !   (because ARR already is allocated and has desired shape), and
+    !   contains the value of STAT returned by the underlying ALLOCATE otherwise.
+    !   In particular, if ARR was successfully allocated STAT has value 0.
+
+integer, dimension(5) :: shp
+integer :: lstat
+
+lstat = -1
+shp = shape(source)
+
+if (allocated(arr)) then
+    if (.not. has_shape (arr, shp)) deallocate (arr)
+end if
+
+if (.not. allocated(arr)) then
+    allocate (arr(shp(1),shp(2),shp(3),shp(4),shp(5)), source=source, stat=lstat)
+end if
+
+if (present(stat)) stat = lstat
