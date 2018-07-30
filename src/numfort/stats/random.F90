d2afdcceb54247c967477902bf9d38e5711659dc
Richard Foltyn <richard.foltyn@gmail.com>
2018-07-30 21:20:17 +0200

    stats: replace with CPP #include directives

--- src/numfort/stats/random.F90
+++ src/numfort/stats/random.F90
@@ -1,7 +1,7 @@
 
-!*  Module containing procedures for generating random numbers that do not
-!   are not implement in any distribution-specific module.
 module numfort_stats_random
+    !*  Module containing procedures for generating random numbers that do not
+    !   are not implement in any distribution-specific module.
 
     use, intrinsic :: iso_fortran_env
     use random, only: random_order_orig => random_order
@@ -19,12 +19,12 @@ contains
 
 subroutine random_order_int32 (x, low)
     integer, parameter :: INTSIZE = int32
-    include "include/random_order_impl.f90"
+#include "random_order_impl.f90"
 end subroutine
 
 subroutine random_order_int64 (x, low)
     integer, parameter :: INTSIZE = int64
-    include "include/random_order_impl.f90"
+#include "random_order_impl.f90"
 end subroutine
 
 end module
