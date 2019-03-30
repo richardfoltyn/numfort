6920ee276b9a1ebcf4489e9e03ac1e506902168b
Richard Foltyn <richard.foltyn@gmail.com>
2019-03-30 21:19:07 +0100

    tests: Cosmetic changes in fixed I/O unit tests

--- src/tests/io/include/io_fixed_impl.F90
+++ src/tests/io/include/io_fixed_impl.F90
@@ -13,17 +13,19 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
     real (PREC), dimension(:,:), allocatable :: dat2d_out, dat2d_in
     real (PREC), dimension(:,:,:), allocatable :: dat3d_out, dat3d_in
     integer :: n, m, k
-    character (100) :: msg
+    type (str) :: msg
+    character (100) :: iomsg
     character (1024) :: tmpdir, path
     character (*), parameter :: filename = 'test_numfort_io_fixed.txt'
     character (20) :: fmt
     type (status_t) :: status
+    logical :: values_ok
 
     msg = 'Unit tests for READ_FIXED / WRITE_FIXED'
     if (PREC == real32) then
-        msg =  trim(msg) // ' (real32)'
+        msg =  msg // ' (real32)'
     else if (PREC == real64) then
-        msg =  trim(msg) // ' (real64)'
+        msg =  msg // ' (real64)'
     end if
 
     tc => tests%add_test (msg)
@@ -40,11 +42,11 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
     call random_number (dat1d_out)
 
     fmt = '(*(5(f8.4),:,/))'
-    call write_fixed (path, fmt, dat1d_out, msg=msg, status=status)
+    call write_fixed (path, fmt, dat1d_out, msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing 1d REAL data in flat form, reshaping via FMT")
 
-    call read_fixed (path, fmt, dat1d_in, msg=msg, status=status)
+    call read_fixed (path, fmt, dat1d_in, msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK .and. &
         all_close (dat1d_out, dat1d_in, atol=1.0e-3_PREC), &
         "Reading 1d REAL data in flat form, reshaping via FMT")
@@ -59,11 +61,11 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
     idat1d_out(:) = int(100.0d0 * rwork1d)
 
     fmt = '(*(5(i3),:,/))'
-    call write_fixed (path, fmt, idat1d_out, msg=msg, status=status)
+    call write_fixed (path, fmt, idat1d_out, msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing 1d INTEGER data in flat form, reshaping via FMT")
 
-    call read_fixed (path, fmt, idat1d_in, msg=msg, status=status)
+    call read_fixed (path, fmt, idat1d_in, msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK .and. &
         all(idat1d_out == idat1d_in), &
         "Reading 1d INTEGER data in flat form, reshaping via FMT")
@@ -79,12 +81,12 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
 
     ! Write in transposed form
     call write_fixed (path, '(*(f10.6))', dat2d_out, transform='transpose', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing file in transposed order")
 
     call read_fixed (path, '(*(f10.6))', dat2d_in, transform='transpose', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK .and. &
         all_close (dat2d_out, dat2d_in, atol=1.0e-5_PREC), &
         "Reading written file in transposed order")
@@ -92,24 +94,24 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
     ! Write in non-transposed order such that one line in the file corresponds
     ! to one row in the array
     call write_fixed (path, '(*(f10.6))', dat2d_out, transform='none', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing file in non-transposed order")
 
     call read_fixed (path, '(*(f10.6))', dat2d_in, transform='none', &
-        msg=msg, status=status)
-    call tc%assert_true (status == NF_STATUS_OK .and. &
-        all_close (dat2d_out, dat2d_in, atol=1.0e-5_PREC), &
+        msg=iomsg, status=status)
+    values_ok= all_close (dat2d_in, dat2d_out, atol=1.0e-4_PREC)
+    call tc%assert_true (status == NF_STATUS_OK .and. values_ok, &
         "Reading written file in non-transposed order")
 
     ! Write in flatten (column-major) order
     call write_fixed (path, '(*(f10.6))', dat2d_out, transform='format', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing file in flatten form")
 
     call read_fixed (path, '(*(f10.6))', dat2d_in, transform='format', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK .and. &
         all_close (dat2d_out, dat2d_in, atol=1.0e-5_PREC), &
         "Reading written file in flatten form")
@@ -117,12 +119,12 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
     ! Test with reshaping via format specifier
     fmt = '(*(2(f10.8),:,/))'
     call write_fixed (path, fmt, dat2d_out, transform='format', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing file in flat form, reshaping via FMT")
 
     call read_fixed (path, fmt, dat2d_in, transform='format', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK .and. &
         all_close (dat2d_out, dat2d_in, atol=1.0e-5_PREC), &
         "Reading written file in flat form, reshaping via FMT")
@@ -137,12 +139,12 @@ subroutine __APPEND(test_fixed,__PREC) (tests)
 
     fmt = '(*(4(f15.8),:,/))'
     call write_fixed (path, fmt, dat3d_out, transform='format', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK, &
         "Writing 3d data in flat form, reshaping via FMT")
 
     call read_fixed (path, fmt, dat3d_in, transform='format', &
-        msg=msg, status=status)
+        msg=iomsg, status=status)
     call tc%assert_true (status == NF_STATUS_OK .and. &
         all_close (dat3d_out, dat3d_in, atol=1.0e-7_PREC), &
         "Reading 3d data in flat form, reshaping via FMT")
@@ -161,16 +163,16 @@ subroutine __APPEND(test_fixed_alloc,__PREC) (tests)
     integer :: nrow, ncol, i
     type (status_t) :: status
     logical :: values_ok
-    character (100) :: msg
+    type (str) :: msg
     character (20) :: fmt
     character (1024) :: tmpdir, path
     character (*), parameter :: filename = 'test_numfort_io_fixed_alloc.txt'
 
     msg = 'Unit tests for READ_FIXED_ALLOC / WRITE_FIXED'
     if (PREC == real32) then
-        msg =  trim(msg) // ' (real32)'
+        msg =  msg // ' (real32)'
     else if (PREC == real64) then
-        msg =  trim(msg) // ' (real64)'
+        msg =  msg // ' (real64)'
     end if
 
     tc => tests%add_test (msg)
