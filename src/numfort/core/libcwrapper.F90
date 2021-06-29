a9f8ce51b0e6dea581dc12c8b8663ac9b296023f
Richard Foltyn <richard.foltyn@gmail.com>
2021-06-29 21:31:21 +0100

    core: Add wrapper for C-library function expm1()

--- src/numfort/core/libcwrapper.F90
+++ src/numfort/core/libcwrapper.F90
@@ -12,6 +12,7 @@ module numfort_core_libc
 
 
     public :: log1p
+    public :: expm1
 
     interface
         function c_log1p_double (value) result(res) bind(C, name='log1p')
@@ -27,12 +28,35 @@ module numfort_core_libc
         end function
     end interface
 
+    interface
+        function c_expm1_double (value) result(res) bind(C, name='expm1')
+            import
+            real (C_DOUBLE), intent(in), value :: value
+            real (C_DOUBLE) :: res
+        end function
+
+        function c_expm1_float (value) result(res) bind(C, name='expm1f')
+            import
+            real (C_FLOAT), intent(in), value :: value
+            real (C_FLOAT) :: res
+        end function
+
+    end interface
+
     interface c_log1p
         procedure c_log1p_float, c_log1p_double
     end interface
 
     interface log1p
         procedure log1p_real32, log1p_real64
+    end interface
+
+    interface c_expm1
+        procedure c_expm1_float, c_expm1_double
+    end interface
+
+    interface expm1
+        procedure expm1_real32, expm1_real64
     end interface
 
     contains
@@ -56,5 +80,24 @@ impure elemental function log1p_real64 (x) result(res)
     res = c_log1p (x)
 end function
 
+
+
+impure elemental function expm1_real32 (x) result(res)
+    !*  EXPM1 implements the EXP(x)-1 function
+    integer, parameter :: PREC = real32
+    real (PREC), intent(in) :: x
+    real (PREC) :: res
+
+    res = c_expm1 (x)
+end function
+
+impure elemental function expm1_real64 (x) result(res)
+    !*  EXPM1 implements the EXP(x)-1 function
+    integer, parameter :: PREC = real64
+    real (PREC), intent(in) :: x
+    real (PREC) :: res
+
+    res = c_expm1 (x)
+end function
 
 end module
