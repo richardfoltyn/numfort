025711c91460808c74cd72919166d58a621e1e13
Richard Foltyn <richard.foltyn@gmail.com>
2024-01-13 16:01:16 +0100

    common: Add COPY_ALLOC for 6d arrays

--- src/numfort/common/copy_alloc.F90
+++ src/numfort/common/copy_alloc.F90
@@ -35,6 +35,11 @@ module numfort_common_copy_alloc
             copy_alloc_5d_int32, copy_alloc_5d_int64
     end interface
 
+    interface copy_alloc
+        procedure copy_alloc_6d_real32, copy_alloc_6d_real64, &
+            copy_alloc_6d_int32, copy_alloc_6d_int64
+    end interface
+
     contains
 
 
@@ -501,5 +506,95 @@ pure subroutine copy_alloc_5d_int64 (src, dst)
 
 end subroutine
 
+
+
+!-------------------------------------------------------------------------------
+! Routines for 6d array
+
+pure subroutine copy_alloc_6d_real32 (src, dst)
+    !*  COPY_ALLOC implements a routine similar to MOVE_ALLOC, but leaves
+    !   the SRC argument unchanged. If the source and destination arrays have
+    !   the same shape, the source data is copied directly into the destination
+    !   array, instead of re-allocating the destination array.
+    !
+    !   Unlike MOVE_ALLOC, COPY_ALLOC does not (and cannot) modify any pointers
+    !   to SRC.
+    !
+    !   If SRC is either missing (or not allocated, which is interpreted as
+    !   not being present in Fortran 2008), DST becomes unallocated
+    !   on exit.
+
+    integer, parameter :: PREC = real32
+    integer, parameter :: ND = 6
+
+    real (PREC), intent(in), dimension(:,:,:,:,:,:), optional :: src
+    real (PREC), intent(out), dimension(:,:,:,:,:,:), allocatable :: dst
+#include "copy_alloc_6d_impl.f90"
+end subroutine
+
+pure subroutine copy_alloc_6d_real64 (src, dst)
+    !*  COPY_ALLOC implements a routine similar to MOVE_ALLOC, but leaves
+    !   the SRC argument unchanged. If the source and destination arrays have
+    !   the same shape, the source data is copied directly into the destination
+    !   array, instead of re-allocating the destination array.
+    !
+    !   Unlike MOVE_ALLOC, COPY_ALLOC does not (and cannot) modify any pointers
+    !   to SRC.
+    !
+    !   If SRC is either missing (or not allocated, which is interpreted as
+    !   not being present in Fortran 2008), DST becomes unallocated
+    !   on exit.
+
+    integer, parameter :: PREC = real64
+    integer, parameter :: ND = 6
+
+    real (PREC), intent(in), dimension(:,:,:,:,:,:), optional :: src
+    real (PREC), intent(out), dimension(:,:,:,:,:,:), allocatable :: dst
+#include "copy_alloc_6d_impl.f90"
+end subroutine
+
+pure subroutine copy_alloc_6d_int32 (src, dst)
+    !*  COPY_ALLOC implements a routine similar to MOVE_ALLOC, but leaves
+    !   the SRC argument unchanged. If the source and destination arrays have
+    !   the same shape, the source data is copied directly into the destination
+    !   array, instead of re-allocating the destination array.
+    !
+    !   Unlike MOVE_ALLOC, COPY_ALLOC does not (and cannot) modify any pointers
+    !   to SRC.
+    !
+    !   If SRC is either missing (or not allocated, which is interpreted as
+    !   not being present in Fortran 2008), DST becomes unallocated
+    !   on exit.
+
+    integer, parameter :: INTSIZE = int32
+    integer, parameter :: ND = 6
+
+    integer (INTSIZE), intent(in), dimension(:,:,:,:,:,:), optional :: src
+    integer (INTSIZE), intent(out), dimension(:,:,:,:,:,:), allocatable :: dst
+#include "copy_alloc_6d_impl.f90"
+
+end subroutine
+
+pure subroutine copy_alloc_6d_int64 (src, dst)
+    !*  COPY_ALLOC implements a routine similar to MOVE_ALLOC, but leaves
+    !   the SRC argument unchanged. If the source and destination arrays have
+    !   the same shape, the source data is copied directly into the destination
+    !   array, instead of re-allocating the destination array.
+    !
+    !   Unlike MOVE_ALLOC, COPY_ALLOC does not (and cannot) modify any pointers
+    !   to SRC.
+    !
+    !   If SRC is either missing (or not allocated, which is interpreted as
+    !   not being present in Fortran 2008), DST becomes unallocated
+    !   on exit.
+
+    integer, parameter :: INTSIZE = int64
+    integer, parameter :: ND = 6
+
+    integer (INTSIZE), intent(in), dimension(:,:,:,:,:,:), optional :: src
+    integer (INTSIZE), intent(out), dimension(:,:,:,:,:,:), allocatable :: dst
+#include "copy_alloc_6d_impl.f90"
+
+end subroutine
 
 end module
